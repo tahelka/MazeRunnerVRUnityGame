bcbf5b6a4e7202062cb8d05942ccdc0dd69d67f8
Matan Cohen <matan10cohen@gmail.com>
2023-09-23 22:09:56 +0300

    Added a body with collider to the player

---
+++ Assets/Scripts/BodyCollision.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyCollision : MonoBehaviour
+{
+    [SerializeField] private Transform m_Head;
+    [SerializeField] private Transform m_Feet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(m_Head.position.x, m_Feet.position.y, m_Head.position.z);
+    }
+}
