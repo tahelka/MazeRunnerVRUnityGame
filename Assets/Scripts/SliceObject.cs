04886939889a97496a4678b7ff323277e82e6d68
Matan Cohen <matan10cohen@gmail.com>
2023-10-16 16:42:17 +0300

    the hammer BREAKS rocks now

--- Assets/Scripts/SliceObject.cs
+++ Assets/Scripts/SliceObject.cs
@@ -7,33 +7,37 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class SliceObject : MonoBehaviour
 {
-    [SerializeField] private Transform m_StartSlicePoint;
-    [SerializeField] private Transform m_EndSlicePoint;
+    [SerializeField] private Transform[] m_StartSlicePoints;
+    [SerializeField] private Transform[] m_EndSlicePoints;
     [SerializeField] private LayerMask m_SliceableLayer;
+    [SerializeField] private string m_SliceableLayerName;
     [SerializeField] private VelocityEstimator m_VelocityEstimator;
     [SerializeField] private Material m_CrossSectionMaterial;
     [SerializeField] private float m_CutForce = 10;
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        bool hasHit = Physics.Linecast(m_StartSlicePoint.position, m_EndSlicePoint.position, out RaycastHit hit, m_SliceableLayer);
-
-        if (hasHit)
+        for (int i = 0; i < m_StartSlicePoints.Length; i++)
         {
-            GameObject target = hit.transform.gameObject;
-            Slice(target);
+            bool hasHit = Physics.Linecast(m_StartSlicePoints[i].position, m_EndSlicePoints[i].position, out RaycastHit hit, m_SliceableLayer);
+
+            if (hasHit)
+            {
+                GameObject target = hit.transform.gameObject;
+                Slice(target, m_StartSlicePoints[i], m_EndSlicePoints[i]);
+            }
         }
     }
 
-    public void Slice(GameObject i_Target)
+
+    public void Slice(GameObject i_Target, Transform startSlicePoint, Transform endSlicePoint)
     {
         Vector3 velocity = m_VelocityEstimator.GetVelocityEstimate();
-        Vector3 planeNormal = Vector3.Cross(m_EndSlicePoint.position - m_StartSlicePoint.position, velocity);
+        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
         planeNormal.Normalize();
-        SlicedHull hull = i_Target.Slice(m_EndSlicePoint.position, planeNormal);
+        SlicedHull hull = i_Target.Slice(endSlicePoint.position, planeNormal);
 
-        if (hull != null) 
+        if (hull != null)
         {
             GameObject upperHull = hull.CreateUpperHull(i_Target, m_CrossSectionMaterial);
             setupSlicedComponent(upperHull);
@@ -43,13 +47,14 @@ public class SliceObject : MonoBehaviour
         }
     }
 
+
     private void setupSlicedComponent(GameObject i_SlicedObject)
     {
         Rigidbody rb = i_SlicedObject.AddComponent<Rigidbody>();
         MeshCollider collider = i_SlicedObject.AddComponent<MeshCollider>();
         i_SlicedObject.AddComponent<XRGrabInteractable>();
         collider.convex = true;
-        i_SlicedObject.layer = LayerMask.NameToLayer("Sliceable");
+        i_SlicedObject.layer = LayerMask.NameToLayer(m_SliceableLayerName);
         rb.AddExplosionForce(m_CutForce, i_SlicedObject.transform.position, 1);
     }
 }
