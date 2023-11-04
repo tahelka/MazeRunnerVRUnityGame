da03ae5a9b7bc3b6a2d70dee69178b7934f880ac
tahelka <73593127+tahelka@users.noreply.github.com>
2023-11-04 12:22:13 +0200

    play only death ot take damage animation

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -20,19 +20,19 @@ public class EnemyManager : MonoBehaviour
         GameObject.Find("EnemiesSpawner").GetComponent<EnemiesSpawnerManager>().DecreaseEnemyCountByOneAndUpdateSpawnTimeNextEnemy();
     }
 
-    public void MakeEnemyStepBack()
-    {
-        // Calculate the backward destination point.
-        Vector3 backwardDestination = transform.position - transform.forward * 2.0f;
+    //public void MakeEnemyStepBack()
+    //{
+    //    // Calculate the backward destination point.
+    //    Vector3 backwardDestination = transform.position - transform.forward * 2.0f;
 
-        // Set the destination for the NavMesh Agent to walk backward.
-        gameObject.GetComponent<NavMeshAgent>().SetDestination(backwardDestination);
-    }
+    //    // Set the destination for the NavMesh Agent to walk backward.
+    //    gameObject.GetComponent<NavMeshAgent>().SetDestination(backwardDestination);
+    //}
 
-    public void MakeEnemyGoAfterPlayer()
-    {
-        GameObject.Find("EnemiesSpawner").GetComponent<EnemiesSpawnerManager>().UpdateEnemyAgentDestinationToMainCamera(gameObject);
-    }
+    //public void MakeEnemyGoAfterPlayer()
+    //{
+    //    GameObject.Find("EnemiesSpawner").GetComponent<EnemiesSpawnerManager>().UpdateEnemyAgentDestinationToMainCamera(gameObject);
+    //}
 
     private void startDyingAnimation()
     {
